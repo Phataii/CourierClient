7c747054b9dce8016ce9438df24cedeb47ec1cb3
phataii <esanni5@gmail.com>
2022-03-13 14:53:39 +0100

    update

--- src/components/auth/LogOutBtn.js
+++ src/components/auth/LogOutBtn.js
@@ -2,18 +2,19 @@ import axios from "axios";
 import React, { useContext } from "react";
 import { useHistory } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
+import requestClient from "../../utils/request-client";
 
 function LogOutBtn() {
-  const { getLoggedIn } = useContext(AuthContext);
+  // const { getLoggedIn } = useContext(AuthContext);
 
   const history = useHistory();
 
   async function logOut() {
-    await axios.get("https://iswds.herokuapp.com/auth/logout");
+    await requestClient.get("/auth/logout");
     // await axios.get(
     //   "https://mern-auth-template-tutorial.herokuapp.com/auth/logout"
     // );
-    await getLoggedIn();
+    // await getLoggedIn();
     history.push("/");
   }
 
